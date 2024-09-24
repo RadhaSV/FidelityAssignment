c43bbd5ac93ce8340716fea5819a233b52d68bb9
RadhaSV <svradha2001@gmail.com>
2024-09-24 22:28:26 +0530

    Add files via upload

---
+++ JAVA/Person.java
@@ -0,0 +1,31 @@
+package small.example;
+
+class Person {
+    private String name;
+    public String gender;
+    public int age;
+
+    Person(String name, String gender, int age) {
+        this.name = name;
+        this.gender = gender;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void displayDetails() {
+        System.out.println("Name: " + getName());
+        System.out.println("Gender: " + gender);
+        System.out.println("Age: " + age);
+    }
+}
+
+class PersonDetails {
+    public static void main(String[] args) {
+        Person person = new Person("Radha", "Female", 23);
+        person.displayDetails();
+    }
+
+}
